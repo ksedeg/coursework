3fa52b26ba5f0d9f9adad415d22e20b7459c3519
root <root@ksedeg-VirtualBox.(none)>
2013-12-24 20:35:27 +0400

    my gallery

---
+++ 3_gallery/images/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Image(models.Model):
+	file=models.ImageField(upload_to='images')
+	title=models.CharField(max_length=200)
+	desc=models.TextField()
---
+++ 3_gallery/images/views.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+from django import forms
+
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+
+from images.models import Image
+from django.template import Context, Template
+
+from django.views.generic import ListView
+from django.views.generic import DetailView
+
+class ImageList(ListView):
+    model = Image
+    context_object_name = 'images'
+    template_name = 'images.html'
+	
+class AddImageForm(forms.Form):
+	file=forms.ImageField()
+	title=forms.CharField(max_length=200)
+	desc=forms.CharField(max_length=200)
+	
+@csrf_exempt
+def AddImage(request):
+	if request.method == 'POST':
+		form = AddImageForm(request.POST, request.FILES)
+		if form.is_valid():
+			i=Image(title=form.cleaned_data['title'], desc=form.cleaned_data['desc'], file=form.cleaned_data['file'])
+			i.save()
+			return HttpResponseRedirect('/')
+	else:
+		form=AddImageForm()
+	
+	return render_to_response('form.html', {
+        'form': form,
+    })
+	
