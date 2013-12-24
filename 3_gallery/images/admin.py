0ecf81c86cbb11ff679da6d225ebc9fff38303b0
root <root@ksedeg-VirtualBox.(none)>
2013-12-24 22:21:59 +0400

    travis errors

--- 3_gallery/images/admin.py
+++ 3_gallery/images/admin.py
@@ -1,8 +1,10 @@
 from images.models import Image
 from django.contrib import admin
 
+
 # Register your models here.
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('title', 'desc', 'file')
 
 admin.site.register(Image, ImageAdmin)
+
--- 3_gallery/images/models.py
+++ 3_gallery/images/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+
 class Image(models.Model):
-	file=models.ImageField(upload_to='images')
-	title=models.CharField(max_length=200)
-	desc=models.TextField()
+    file=models.ImageField(upload_to='images')
+    title=models.CharField(max_length=200)
+    desc=models.TextField()
+
